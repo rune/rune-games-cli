06d4dffe9bb445b303a4c89e3f093bc91285f812
Helios1138 <revan.den@gmail.com>
2022-04-26 14:41:31 +0300

    temp

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,103 @@
-import React from "react"
+import React, { useEffect, useMemo } from "react"
 import { render, Text } from "ink"
+import getPort from "get-port"
+
+import express from "express"
+import fs from "fs"
+import path from "path"
+import url from "url"
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+console.log({ __filename, __dirname, url: import.meta.url })
+
+getPort()
+  .then((port) => {
+    console.log("getPort", port)
+  })
+  .catch((err) => {
+    console.log("error", err)
+  })
+
+const packageJson = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "../package.json"), "utf8")
+)
+
+// const app = express()
+// const port = 3000
+
+// todo: test sdk on one port, game on second and wrapper on third
+
+// todo: perhaps just serve game and wrapper on different ports always? to avoid clearLocalStorage having effect on wrapper
+
+// app.use(
+//   "/sdk",
+//   express.static("/Users/helios/Projects/rune/rune-games-sdk/dist")
+// )
+//
+// app.use(
+//   "/__game__",
+//   express.static("/Users/helios/Projects/rune/rune-games-cli/__temp/5")
+// )
+
+// app.use(
+//   "/__build*",
+//   express.static("/Users/helios/Projects/rune/rune-games-cli/__temp/5", {
+//     // redirect: false,
+//   })
+// )
+
+// app.use("/", express.static("/Users/helios/Projects/rune/TinCan/app/web/build"))
+//
+// app.use("*", (_, res) => {
+//   res.sendFile("/Users/helios/Projects/rune/TinCan/app/web/build/index.html")
+// })
+//
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
 
 const Counter = () => {
+  const appServer = useMemo(() => express(), [])
+  const gameServer = useMemo(() => express(), [])
+  const sdkServer = useMemo(() => express(), [])
+
+  useEffect(() => {
+    appServer.get("/data", (_, res) => {
+      // todo: remove
+      res.setHeader("Access-Control-Allow-Origin", "*")
+      res.json({
+        cliVersion: packageJson.version,
+        gameUrl: "https://games-launchpad.rune.ai/13/16/",
+      })
+    })
+    appServer.use(
+      "/",
+      express.static("/Users/helios/Projects/rune/TinCan/app/web/build")
+    )
+    appServer.use("*", (_, res) => {
+      res.sendFile(
+        "/Users/helios/Projects/rune/TinCan/app/web/build/index.html"
+      )
+    })
+    appServer.listen(3001, () => {
+      console.log("listening")
+    })
+
+    gameServer.use(
+      "/",
+      express.static("/Users/helios/Projects/rune/rune-games-cli/__temp/5")
+    )
+    gameServer.listen(3002)
+
+    sdkServer.use(
+      "/",
+      express.static("/Users/helios/Projects/rune/rune-games-sdk/dist")
+    )
+    sdkServer.listen(3003)
+  }, [])
+
   return <Text>Hello</Text>
 }
 
