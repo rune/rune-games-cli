bdcfd69c485667e642a1a155eb12a31b2682afc8
Helios1138 <revan.den@gmail.com>
2022-07-26 19:42:12 +0300

    slightly better list ui (still temporary)

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,4 +1,4 @@
-import { Text } from "ink"
+import { Text, Box } from "ink"
 import React from "react"
 
 import { useGames } from "../gql/useGames.js"
@@ -8,5 +8,13 @@ export function List() {
   const { me } = useMe()
   const { games } = useGames({ skip: !me, condition: { devTeamId: me?.id } })
 
-  return <Text>{JSON.stringify({ me, games })}</Text>
+  return (
+    <Box flexDirection="column">
+      <Text>Your games:</Text>
+      {!games?.length && <Text>You have not submitted any games yet</Text>}
+      {games?.map((game) => (
+        <Text key={game.id}>- {game.title}</Text>
+      ))}
+    </Box>
+  )
 }
