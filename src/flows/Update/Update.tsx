af9068e3a4e54c99952e3408224ae846a720c1e0
Helios1138 <revan.den@gmail.com>
2022-10-05 18:38:07 +0300

    update all games

--- src/flows/Update/Update.tsx
+++ src/flows/Update/Update.tsx
@@ -3,10 +3,13 @@ import React, { useState } from "react"
 
 import { ChooseGameStep } from "../Upload/ChooseGameStep.js"
 
+import { UpdateAll } from "./UpdateAll.js"
 import { UpdateGameStep } from "./UpdateGameStep.js"
 
-export function Update() {
+export function Update({ args }: { args: string[] }) {
   const [gameId, setGameId] = useState<number | null | undefined>()
+
+  if (args[0] === "all") return <UpdateAll />
 
   return (
     <Box flexDirection="column">
---
+++ src/flows/Update/UpdateAll.tsx
@@ -0,0 +1,66 @@
+import { Box, Text } from "ink"
+import React, { useEffect } from "react"
+
+import { Step } from "../../components/Step.js"
+import { useUpdateAllGames } from "../../gql/useUpdateAllGames.js"
+import { formatApolloError } from "../../lib/formatApolloError.js"
+
+export function UpdateAll() {
+  const {
+    updateAllGames,
+    updateAllGamesLoading,
+    updateAllGamesError,
+    updateAllGamesResult,
+  } = useUpdateAllGames()
+
+  useEffect(() => {
+    updateAllGames()
+  }, [updateAllGames])
+
+  return (
+    <Step
+      status={
+        updateAllGamesLoading
+          ? "waiting"
+          : updateAllGamesError
+          ? "error"
+          : "success"
+      }
+      label={
+        updateAllGamesLoading
+          ? "Updating all games. This may take a while. Do not close the app"
+          : updateAllGamesError
+          ? formatApolloError(updateAllGamesError, {
+              default: updateAllGamesError.message,
+            })
+          : "Finished updating all games"
+      }
+      view={
+        <Box flexDirection="column">
+          {!!updateAllGamesResult?.updatedGameIds.length && (
+            <Text color="green">
+              Successfully updated games:{" "}
+              {updateAllGamesResult.updatedGameIds.join(", ")}
+            </Text>
+          )}
+          {!!updateAllGamesResult?.failedToUpdateGameIds.length && (
+            <Text color="red">
+              Failed to update games:{" "}
+              {updateAllGamesResult.failedToUpdateGameIds.join(", ")}
+            </Text>
+          )}
+          {!!updateAllGamesResult?.errors.length && (
+            <>
+              <Text color="red">Errors:</Text>
+              {updateAllGamesResult.errors.map((error, index) => (
+                <Text key={index} color="red">
+                  {error}
+                </Text>
+              ))}
+            </>
+          )}
+        </Box>
+      }
+    />
+  )
+}
