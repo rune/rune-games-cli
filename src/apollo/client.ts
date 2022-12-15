1d23b83986fd3c85bfb610e26a4c101b2a174869
Joel Arvidsson <joel@rune.ai>
2022-12-15 10:01:37 +0100

    Retire this repo (#33)

--- src/apollo/client.ts
+++
@@ -1,9 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client/index.js"
-
-import { contextLink } from "./contextLink.js"
-import { uploadLink } from "./uploadLink.js"
-
-export const client = new ApolloClient({
-  link: contextLink.concat(uploadLink),
-  cache: new InMemoryCache(),
-})
--- src/lib/rootPath.ts
+++
@@ -1,7 +0,0 @@
-import path from "path"
-import url from "url"
-
-export const rootPath = path.resolve(
-  path.dirname(url.fileURLToPath(import.meta.url)),
-  "../.."
-)
